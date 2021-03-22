785f84b453ef293d54e4717505c0eb193cd215a5
EvalunaC <73672863+EvalunaC@users.noreply.github.com>
2021-03-22 16:43:33 -0500

    Update pipelineTesting_Lapatinib.R

--- pipelineTesting_Lapatinib.R
+++ pipelineTesting_Lapatinib.R
@@ -1,1 +1,12 @@
-A
+set.seed(1000)
+memory.limit(9999999999)
+
+load("C:/Users/qiangli/Desktop/Drug Response Project/DR_Pred/trainFrame.RData")
+
+library(caret)
+library(pRRophetic)
+library(ridge)
+library(MASS)
+library(dplyr)
+library(caret)
+library(glmnet)
