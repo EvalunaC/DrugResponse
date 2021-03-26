b474b3ccc2cc4bee2d54a104ea5c2b96fef03600
EvalunaC <73672863+EvalunaC@users.noreply.github.com>
2021-03-26 06:31:52 -0500

    Update Looping.R

--- Looping.R
+++ Looping.R
@@ -1,1 +1,29 @@
 
+
+
+
+length(possibleDrugs2)
+drug_data <- getDrugData(possibleDrugs2[1])
+drug_result<- methods_result(drug_data, possibleDrugs2[1])
+write.csv(df, "/extraspace/ychen42/Drug_Response/yiqings_work/Output/192Drug_MethodsResult.csv",
+          append = TRUE,
+          col.names = FALSE,
+          row.names = TRUE,
+          quote = FALSE)
+for (i in 2:length(possibleDrugs2)){
+  drug_data <- getDrugData(possibleDrugs2[i])
+  drug_result<- methods_result(drug_data, possibleDrugs2[i])
+    write.csv(df, "/extraspace/ychen42/Drug_Response/yiqings_work/Output/192Drug_MethodsResult.csv",
+          col.names = TRUE,
+          row.names = TRUE,
+          quote = FALSE)
+ 
+  }
+
+#name <- paste("/extraspace/ychen42/Drug_Response/yiqings_work/Output/",drug,".csv", sep="",collapse = NULL)
+#write.csv(df,name, quote=FALSE)
+
+
+
+
+
