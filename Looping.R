8773ef4a0f8c803c99f3d0cfb1c32cf15ae62f32
EvalunaC <73672863+EvalunaC@users.noreply.github.com>
2021-03-26 20:57:43 -0500

    Update Looping.R

--- Looping.R
+++ Looping.R
@@ -1,22 +1,35 @@
+library(doMC)
+options(cores = 20)
+registerDoMC()
 
+library(parallel)
 
-
-
-length(possibleDrugs2)
+#length(possibleDrugs2)
 drug_data <- getDrugData(possibleDrugs2[1])
 drug_result<- methods_result(drug_data, possibleDrugs2[1])
 write.csv(df, "/extraspace/ychen42/Drug_Response/yiqings_work/Output/192Drug_MethodsResult.csv",
           col.names = FALSE,
           row.names = TRUE,
           quote = FALSE)
-for (i in 2:length(possibleDrugs2)){
-  drug_data <- getDrugData(possibleDrugs2[i])
-  drug_result<- methods_result(drug_data, possibleDrugs2[i])
-    write.csv(df, "/extraspace/ychen42/Drug_Response/yiqings_work/Output/192Drug_MethodsResult.csv",
-          append = TRUE,
+
+drug_data <- getDrugData(possibleDrugs2[3])
+drug_result<- methods_result(drug_data, possibleDrugs2[3])
+cat(drug_result)
+write.csv(df, "/extraspace/ychen42/Drug_Response/yiqings_work/Output/192Drug_MethodsResult_Cisplatin.csv",
           col.names = TRUE,
           row.names = TRUE,
           quote = FALSE)
+
+
+for (i in 2:length(possibleDrugs2)){
+  drug_data <- getDrugData(possibleDrugs2[i])
+  methods_result(drug_data, possibleDrugs2[i])
+#  drug_result<- methods_result(drug_data, possibleDrugs2[i])
+#    write.csv(df, "/extraspace/ychen42/Drug_Response/yiqings_work/Output/192Drug_MethodsResult.csv",
+#          append = TRUE,
+#          col.names = TRUE,
+#          row.names = TRUE,
+#          quote = FALSE)
  
   }
 
